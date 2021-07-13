aea2e8490fc29d7e4fa397dcdcfe4407da5aadf3
Roger Takeshita <roger.takeshita@gmail.com>
2021-07-12 22:56:57 -0400

    REACT-15 Should update navbar to be responsive and page width

--- src/components/base/Navbar.tsx
+++ src/components/base/Navbar.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
 import { logoutUser } from '../../redux/user';
@@ -6,35 +8,41 @@ import * as Type from '../../utils/@types/types';
 
 const Navbar: React.FC = () => {
     const user = useSelector((state: RootStateOrAny) => state.user);
+    const [visible, setVisible] = useState(false);
     const dispatch = useDispatch();
     const history = useHistory();
 
     const handleLogout: Type.HandleClickFn = (e) => {
         e.preventDefault();
         dispatch(logoutUser());
+        setVisible(false);
         history.push('/');
+    };
+
+    const handleClick: Type.HandleClickFn = () => {
+        setVisible((prev) => !prev);
     };
 
     const menu =
         user && user.firstName ? (
             <ul className="navbar__list">
                 <li className="navbar__item">
-                    <Link className="navbar__link" to="/">
+                    <Link className="navbar__link" to="/" onClick={() => setVisible(false)}>
                         HOME
                     </Link>
                 </li>
                 <li className="navbar__item">
-                    <Link className="navbar__link" to="/about">
+                    <Link className="navbar__link" to="/about" onClick={() => setVisible(false)}>
                         ABOUT
                     </Link>
                 </li>
                 <li className="navbar__item">
-                    <Link className="navbar__link" to="/api">
+                    <Link className="navbar__link" to="/api" onClick={() => setVisible(false)}>
                         API
                     </Link>
                 </li>
                 <li className="navbar__item">
-                    <Link className="navbar__link" to="/profile">
+                    <Link className="navbar__link" to="/profile" onClick={() => setVisible(false)}>
                         PROFILE
                     </Link>
                 </li>
@@ -47,22 +55,22 @@ const Navbar: React.FC = () => {
         ) : (
             <ul className="navbar__list">
                 <li className="navbar__item">
-                    <Link className="navbar__link" to="/">
+                    <Link className="navbar__link" to="/" onClick={() => setVisible(false)}>
                         HOME
                     </Link>
                 </li>
                 <li className="navbar__item">
-                    <Link className="navbar__link" to="/about">
+                    <Link className="navbar__link" to="/about" onClick={() => setVisible(false)}>
                         ABOUT
                     </Link>
                 </li>
                 <li className="navbar__item">
-                    <Link className="navbar__link" to="/login">
+                    <Link className="navbar__link" to="/login" onClick={() => setVisible(false)}>
                         LOGIN
                     </Link>
                 </li>
                 <li className="navbar__item">
-                    <Link className="navbar__link" to="/signup">
+                    <Link className="navbar__link" to="/signup" onClick={() => setVisible(false)}>
                         SIGN UP
                     </Link>
                 </li>
@@ -71,10 +79,22 @@ const Navbar: React.FC = () => {
 
     return (
         <div className="navbar">
-            <Link className="navbar__logo" to="/">
-                Roger Takeshita
-            </Link>
-            <div className="navbar__menu-container">{menu}</div>
+            <div className="navbar__navbar-container">
+                <div
+                    onClick={handleClick}
+                    className={visible ? 'navbar__bars navbar__bars--light' : 'navbar__bars navbar__bars--dark'}
+                >
+                    <FontAwesomeIcon icon={faBars} />
+                </div>
+                <div className="navbar__logo-container">
+                    <Link className="navbar__logo" to="/" onClick={() => setVisible(false)}>
+                        Roger Takeshita
+                    </Link>
+                </div>
+                <div className={`navbar__menu-container ${visible ? 'navbar__menu-container--visible' : ''}`}>
+                    {menu}
+                </div>
+            </div>
         </div>
     );
 };
