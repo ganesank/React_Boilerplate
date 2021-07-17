aa99f887a3ec9e5d562f0cd17da48207ade85595
Roger Takeshita <roger.takeshita@gmail.com>
2021-07-17 12:32:39 -0400

    REACT-18 Should dry the popup component to use Button component

--- src/components/shared/Popup.tsx
+++ src/components/shared/Popup.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useSelector, useDispatch, RootStateOrAny } from 'react-redux';
+import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
 import { hidePopup } from '../../redux/popup';
 import * as Type from '../../utils/@types/types';
+import Button from '../shared/Button';
 
 const Popup: React.FC = ({ children }) => {
     const popup = useSelector((state: RootStateOrAny) => state.popup);
@@ -15,9 +16,7 @@ const Popup: React.FC = ({ children }) => {
     return (
         <div className="popup" onClick={handleClose}>
             <div className="popup__container" onClick={(e) => e.stopPropagation()}>
-                <a href="/" className="popup__close" onClick={handleClose}>
-                    x
-                </a>
+                <Button btnType="link" value="x" href="/" handle="popup__close" onClick={handleClose} />
                 {popup.title !== '' && (
                     <div className="popup__header">
                         <h3>{popup.title}</h3>
